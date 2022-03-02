ec00708d0ec411c23d6e8853a245d817758884a9
Jlove <j.love36314@gmail.com>
2022-03-01 20:01:39 -0500

    m

--- app.py
+++ app.py
@@ -24,15 +24,28 @@ if __name__ == "__main__":
 else:
     print("example is being imported")
 
+    app = Flask(__name__)
+    
     @app.route("/")
+
     def welcome():
 
         return(
         '''
-        Welcome to the Climate Analysis API!
-        Available Routes:
-        /api/v1.0/precipitation
-        /api/v1.0/stations
-        /api/v1.0/tobs
-        /api/v1.0/temp/start/end
+        Welcome to the Climate Analysis API! 
+        Available Routes: 
+        /api/v1.0/precipitation 
+        /api/v1.0/stations 
+        /api/v1.0/tobs 
+        /api/v1.0/temp/start/end 
         ''')
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
